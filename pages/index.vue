d024d2dc79111726fe2f5c34803690966b7ffe14
wintyo <wintyo1024@gmail.com>
2020-01-29 14:36:33 +0900

    差分ファイルをZip圧縮してダウンロードする

--- pages/index.vue
+++ pages/index.vue
@@ -28,11 +28,16 @@ div
             }"
           )
             | {{ fileInfo.fileName }}
+  div
+    button(@click="onDownloadButtonClick")
+      | 差分ダウンロード
 </template>
 
 <script>
 import Vue from 'vue';
 const { Unzip } = require('zlibjs/bin/unzip.min').Zlib;
+const { Zip } = require('zlibjs/bin/zip.min').Zlib;
+import File from '../utils/File';
 
 /**
  * 配列データが一緒か調べる
@@ -52,6 +57,33 @@ function checkSameArray(arr1, arr2) {
   }
 
   return true;
+}
+
+function strToUtf8Array(str) {
+  var n = str.length,
+    idx = -1,
+    bytes = [],
+    i, j, c;
+
+  for (i = 0; i < n; ++i) {
+    c = str.charCodeAt(i);
+    if (c <= 0x7F) {
+      bytes[++idx] = c;
+    } else if (c <= 0x7FF) {
+      bytes[++idx] = 0xC0 | (c >>> 6);
+      bytes[++idx] = 0x80 | (c & 0x3F);
+    } else if (c <= 0xFFFF) {
+      bytes[++idx] = 0xE0 | (c >>> 12);
+      bytes[++idx] = 0x80 | ((c >>> 6) & 0x3F);
+      bytes[++idx] = 0x80 | (c & 0x3F);
+    } else {
+      bytes[++idx] = 0xF0 | (c >>> 18);
+      bytes[++idx] = 0x80 | ((c >>> 12) & 0x3F);
+      bytes[++idx] = 0x80 | ((c >>> 6) & 0x3F);
+      bytes[++idx] = 0x80 | (c & 0x3F);
+    }
+  }
+  return bytes;
 }
 
 export default Vue.extend({
@@ -119,6 +151,20 @@ export default Vue.extend({
       };
 
       reader.readAsArrayBuffer(file);
+    },
+    onDownloadButtonClick() {
+      const zip = new Zip();
+
+      this.$data.zipFileInfoListB
+        .filter((fileInfo) => ['add', 'change'].includes(this._diffZipFileMap[fileInfo.fileName]))
+        .forEach((fileInfo) => {
+          zip.addFile(fileInfo.binaryData, {
+            filename: strToUtf8Array(fileInfo.fileName),
+          });
+        });
+      const compressData = zip.compress();
+
+      File.download('sample.zip', compressData, 'application/zip');
     },
   },
 });
---
+++ utils/File.js
@@ -0,0 +1,34 @@
+const BOM = new Uint8Array([0xEF, 0xBB, 0xBF]);  // UTF-8のBOM
+
+class File {
+  /**
+   * ファイルにダウンロードする
+   * @param {string} fileName - ファイル名
+   * @param {string|Uint8Array} contentStr - コンテンツの中身
+   * @param {string} contentType - コンテンツのタイプ
+   */
+  static download(fileName, contentStr, contentType = 'text/plain') {
+    const data = (() => {
+      if (contentType === 'text/plain' || contentType === 'text/csv') {
+        return [BOM, contentStr];
+      }
+      return [contentStr];
+    })();
+    const blob = new Blob(data, { type: contentType });
+
+    // IE11, 10対応
+    if (window.navigator.msSaveOrOpenBlob) {
+      // msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+      window.navigator.msSaveOrOpenBlob(blob, fileName);
+      return;
+    }
+
+    const element = document.createElement('a');
+    element.href = window.URL.createObjectURL(blob);
+    element.setAttribute('download', fileName);
+    element.click();
+    window.URL.revokeObjectURL(element.href);
+  }
+}
+
+export default File;
