77cfd273a7d39cc6369dcd7ba41cc6d8957ffd8c
wintyo <wintyo1024@gmail.com>
2020-02-02 16:37:32 +0900

    GitHub Actionsを使ってGitHub Pagesに登録する

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,11 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/diff-zip-file/'
+  }
+} : {};
 
 export default {
+  ...routerBase,
   mode: 'spa',
   /*
   ** Headers of the page
